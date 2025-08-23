dc87e58eaed7b2efe15bdf2d1adddcda22ec2e73
Eduardo Pereira <dudupererasiva@gmail.com>
2025-08-22 21:51:07 -0300

    Começo das atividades com Java Script

---
+++ javaScript/condisionais.js
@@ -0,0 +1,18 @@
+// Exercicio 1
+
+let idade = prompt("Qual e a idade? ");
+
+if( idade > 18){
+
+alert("Paraben, vc é maior de idade")
+
+} else {
+
+alert("Opa, vc é manor de idade")
+
+}
+
+// Exercicio 2
+
+let numero = prompt(" digite um numero")
+
---
+++ javaScript/script.js
@@ -0,0 +1,18 @@
+// console.log("Hello World!");
+
+// let nome = prompt("Qual seu nome?") ;
+// let idade = prompt("Qual sua idade?") ;
+
+// alert( "Meu nome é " +  nome + " e tenho " + idade + " anos");
+
+let idade = prompt("Qual e a idade? ");
+
+if( idade > 18){
+
+alert("Paraben, vc é maior de idade")
+
+} else {
+
+alert("Opa, vc é manor de idade")
+
+}
